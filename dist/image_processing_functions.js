166b4cfbd9493d0c372d56feebb211b4c98e9ca7
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-11-18 15:19:08 +0100

    Update image_processing_functions.js

--- dist/image_processing_functions.js
+++ dist/image_processing_functions.js
@@ -84,65 +84,33 @@ async function modify_canvas_data(normalArray) {
 	
 // -------------------------------------------------
 
-async function convert_canvas_to_base64str() {
-
-	// -----------------------------
-
-	// Put image on a canvas
-	await gradient();
-
+async function convert_canvas_to_base64str(ctx, canvasElement) {
+	
 	// -----------------------------
 	
 	// Get canvas image
-	const uint8ClampedArray = await ctx.getImageData(0, 0, width, height); // TypedArray  Uint8ClampedArray
-	console.log('uint8ClampedArray: ', uint8ClampedArray);
+	const uint8ClampedArray = await ctx.getImageData(0, 0, canvasElement.width, canvasElement.height); // TypedArray  Uint8ClampedArray
+	// console.log('uint8ClampedArray: ', uint8ClampedArray);
 
 	// Convert TypedArray to normalArray
 	const normalArray = await Array.from(uint8ClampedArray.data);
-	console.log('normalArray: ', normalArray);
-	// length normalArray:  262144
+	// console.log('normalArray: ', normalArray);
 	
 	// Convert [normalArray of bytes] to text_characters
 	var ASCII_stringArray = normalArray.map((val) => { return String.fromCharCode(val); });
-	console.log('ASCII_stringArray: ', ASCII_stringArray);
-	// length 262144
+	// console.log('ASCII_stringArray: ', ASCII_stringArray);
 	
 	// Join all the ASCII characters to form a string
 	var ASCII_str = ASCII_stringArray.join('');
-	console.log("ASCII_str: ", ASCII_str);
+	// console.log("ASCII_str: ", ASCII_str);
 	
-	// Put header on base64_string (optional)
-	
-		
 	// Transform ASCII string into a base64 string
 	var base64_string = btoa(ASCII_str);
-	console.log("base64_string: ", base64_string);
+	// console.log("base64_string: ", base64_string);
 
 	// -----------------------------
 
-	// -----------------------------
-	// Put the base64string onto another canvas to confirm that it is correct, and can be reconstructed
-	// -----------------------------
-	var ASCII_str = atob(base64_string);
-	console.log("base64_string: ", base64_string);
-	
-	// Transform base64_string into Typed array
-	var character_array = ASCII_str.split('');
-	console.log("character_array: ", character_array);
-	// Array(38190) [ "�", "�", "�", "d", "\u0000", "\u0000", "\u0000", "\u0000", "\u0000", "\u0000", … ]
-	// length = 349528
-			
-	// Map each [binary string character; a subset of binary string characters is UTF-8] as an [ASCII number; a number from 0 to number_of_characters]
-	var byteArray = character_array.map((character) => { return character.charCodeAt(0); });
-	console.log("byteArray: ", byteArray);
-	
-	// The importance of this mapping is to limit the array values from 0 to 255.
-	const uint8_typedarray = await new Uint8ClampedArray(byteArray);
-	console.log('uint8_typedarray: ', uint8_typedarray);
-	
-	let imageData = await new ImageData(uint8_typedarray, width, height);
-	document.getElementById("canvas_img_r").style.left = Number(1*(width + 40)).toString() + 'px';
-	ctx1.putImageData(imageData, 0, 0);
+	return [ASCII_str, base64_string];
 }
 
 // -------------------------------------------------
