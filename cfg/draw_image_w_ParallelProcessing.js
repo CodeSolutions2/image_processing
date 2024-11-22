b3b409935a5570a68633c2b5b3f7e576a011f2ac
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-11-22 15:37:43 +0100

    Update draw_image_w_ParallelProcessing.js

--- cfg/draw_image_w_ParallelProcessing.js
+++ cfg/draw_image_w_ParallelProcessing.js
@@ -1,23 +1,32 @@
 // draw_image_w_ParallelProcessing.js
 
-const canvasElement = document.querySelector('.canvasElement_className');
-
 // self refers to the global object in the browser
 self.onmessage = function(event) {
 
-	console.log("draw_image_w_ParallelProcessing.js - event.data.type: ", event.data.type);  	// process
-	console.log("draw_image_w_ParallelProcessing.js - event.data.data: ", event.data.data);	// Typedarray
-	console.log("draw_image_w_ParallelProcessing.js - event: ", event);  	// [object MessageEvent]
+	console.log("draw_image_w_ParallelProcessing.js - event.data.canvas: ", event.data.canvas);
+	console.log("draw_image_w_ParallelProcessing.js - event.data.width: ", event.data.width);
+	console.log("draw_image_w_ParallelProcessing.js - event.data.height: ", event.data.height);
+	console.log("draw_image_w_ParallelProcessing.js - event: ", event);
 	
-	if (event.data.type == 'process') {
+	// receive and set offscreen canvas
+	const off_canvasElement = event.data.canvas;
+
+	self.onmessage = function(event) {
+		console.log("draw_image_w_ParallelProcessing.js - event.data.type: ", event.data.type);  	// process
+		console.log("draw_image_w_ParallelProcessing.js - event.data.data: ", event.data.data);	// Typedarray
+		console.log("draw_image_w_ParallelProcessing.js - event: ", event);  	// [object MessageEvent]
 		
-		// Need to put the Typedarray on to the existing canvas from index.html
+		if (event.data.type == 'process') {
+			
+			// Need to put the Typedarray on to the existing canvas from index.html
+	
+			// Draw parallel processed data on canvas
+			const imageData = new ImageData(event.data.data, canvasElement.width, canvasElement.height);
+			off_canvasElement.getContext("2d").putImageData(imageData, 0, 0);
+	
+			// Send processed data back to main thread
+			self.postMessage("Modification Drawn on Canvas: DONE");
+		}
 
-		// Draw parallel processed data on canvas
-		const imageData = new ImageData(event.data.data, canvasElement.width, canvasElement.height);
-		canvasElement.getContext("2d").putImageData(imageData, 0, 0);
-
-		// Send processed data back to main thread
-		self.postMessage("Modification Drawn on Canvas: DONE");
 	}
 }
