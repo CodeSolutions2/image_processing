e3cbe04bdef737a3dfdc09c8c7b53309270ca741
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-11-22 16:02:38 +0100

    Update draw_image_w_ParallelProcessing.js

--- cfg/draw_image_w_ParallelProcessing.js
+++ cfg/draw_image_w_ParallelProcessing.js
@@ -2,34 +2,22 @@
 
 const canvasElement = document.querySelector('.canvasElement_className');
 
-
 // self refers to the global object in the browser
-// self.onmessage = function(event) {
-
-	// console.log("draw_image_w_ParallelProcessing.js - event.data.canvas: ", event.data.canvas);
-	// console.log("draw_image_w_ParallelProcessing.js - event.data.width: ", event.data.width);
-	// console.log("draw_image_w_ParallelProcessing.js - event.data.height: ", event.data.height);
-	// console.log("draw_image_w_ParallelProcessing.js - event: ", event);
+self.onmessage = function(event) {
+	console.log("draw_image_w_ParallelProcessing.js - event.data.type: ", event.data.type);  	// process
+	console.log("draw_image_w_ParallelProcessing.js - event.data.data: ", event.data.data);	// Typedarray
+	console.log("draw_image_w_ParallelProcessing.js - event: ", event);  	// [object MessageEvent]
 	
-	// receive and set offscreen canvas
-	// const off_canvasElement = event.data.canvas;
-
-	self.onmessage = function(event) {
-		console.log("draw_image_w_ParallelProcessing.js - event.data.type: ", event.data.type);  	// process
-		console.log("draw_image_w_ParallelProcessing.js - event.data.data: ", event.data.data);	// Typedarray
-		console.log("draw_image_w_ParallelProcessing.js - event: ", event);  	// [object MessageEvent]
+	if (event.data.type == 'process') {
 		
-		if (event.data.type == 'process') {
-			
-			// Need to put the Typedarray on to the existing canvas from index.html
-	
-			// Draw parallel processed data on canvas
-			const imageData = new ImageData(event.data.data, canvasElement.width, canvasElement.height);
-			canvasElement.getContext("2d").putImageData(imageData, 0, 0);
-	
-			// Send processed data back to main thread
-			self.postMessage("Modification Drawn on Canvas: DONE");
-		}
+		// Need to put the Typedarray on to the existing canvas from index.html
 
+		// Draw parallel processed data on canvas
+		const imageData = new ImageData(event.data.data, canvasElement.width, canvasElement.height);
+		canvasElement.getContext("2d").putImageData(imageData, 0, 0);
+
+		// Send processed data back to main thread
+		self.postMessage("Modification Drawn on Canvas: DONE");
 	}
-// }
+
+}
