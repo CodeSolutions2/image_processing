39f5465fd3a5075cc0e25d8b7c78eee5e5983c49
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-11-26 17:23:42 +0100

    Update retrieve_image.js

--- cfg/retrieve_image.js
+++ cfg/retrieve_image.js
@@ -7,38 +7,51 @@ self.onmessage = function(event) {
 
 	// event.data.data
 	var url = "https://storage.googleapis.com/on-the-way2selfrespect/yellow.png";
+
+
+	// Determine request method
+	var headers = {
+		"Content-Type": "application/json",
+		"Referer": url,
+		"Origin": "https://codesolutions2.github.io",
+		"Connection": "keep-alive",
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
+		"Access-Control-Allow-Origin": "*"
+	};
 	
-	if (event.data.type == 'process') {
+	var options = { 
+		method : "GET",
+		mode: 'cors',
+		headers: new Headers(headers),
+		cache: "no-cache",
+		crossorigin: 'anonymous',
+		redirect: "follow"
+	};
+	
+	// Fetch data
+	fetch(url, options)
+		.then(response => response.arrayBuffer())
+		.then(async function(arraybuffer) {
+			console.log("arraybuffer: ", arraybuffer);
+			
+			// Send processed data back to main thread
+			self.postMessage(arraybuffer);
+		})
+		.catch(error => console.error("error: ", error));
+	
 
-		// Determine request method
-		var headers = {
-			"Content-Type": "application/json",
-			"Referer": url,
-			"Origin": "https://codesolutions2.github.io",
-			"Connection": "keep-alive",
-			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
-			"Access-Control-Allow-Origin": "*"
-		};
-		
-		var options = { 
-			method : "GET",
-			mode: 'cors',
-			headers: new Headers(headers),
-			cache: "no-cache",
-			crossorigin: 'anonymous',
-			redirect: "follow"
-		};
-		
-		// Fetch data
-		const arraybuffer = fetch(url, options)
-				.then(response => response.arrayBuffer())
-				.then(async function(arraybuffer) { return arraybuffer; })
-				.catch(error => console.error("error: ", error));
-		console.log("arraybuffer: ", arraybuffer);
+	// OR
+	
+	fetch(url, options)
+		.then(response => response.blob())
+		.then(async function(blob) {
+			const url = URL.createObjectURL(blob);
+			console.log("arraybuffer: ", arraybuffer);
+
+			self.postMessage(url);
+		})
+		.catch(error => console.error("error: ", error));
 		
 
-		// Send processed data back to main thread
-		self.postMessage(arraybuffer);
-	}
 
 }
